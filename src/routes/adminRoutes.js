48ec7d5ea9011c5d662b792b552c00b15e4e26ac
Yodahe Teshome <jodahe2001@gmail.com>
2025-04-04 11:03:16 +0300

    restructuring again

--- src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addAdmin } = require('../controllers/adminController');
-
-// Route to add a new administrator
-router.post('/', addAdmin);
-
-module.exports = router;
